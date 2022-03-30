64fa432a2ae286c367c036d19c93109c1cafc4bd
王叶盛 <2667009323@qq.com>
2022-03-30 22:12:07 +0800

    实现了动态SQL查询

--- src/main/java/com/itheima/mapper/BrandMapper.java
+++ src/main/java/com/itheima/mapper/BrandMapper.java
@@ -1,11 +1,17 @@
 package com.itheima.mapper;
 
 import com.itheima.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
     List<Brand> selectAll();
 
     Brand selectById(int id);
+
+    List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
+
+    List<Brand> selectByCondition(Map map);
 }
--- src/test/java/com/itheima/test/MyBatisTest.java
+++ src/test/java/com/itheima/test/MyBatisTest.java
@@ -10,7 +10,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyBatisTest {
 
@@ -51,6 +53,36 @@ public class MyBatisTest {
         //4.执行方法
         Brand brand = brandMapper.selectById(1);
         System.out.println(brand);
+
+        //5.释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectByCondition() throws IOException {
+        int status = 1;
+        String companyName = "%" + "华为" + "%";
+        String brandName = "%" + "华为" + "%";
+
+        Map map = new HashMap();
+        map.put("status",status);
+//        map.put("companyName",companyName);
+//        map.put("brandName",brandName);
+
+        //1.获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2.获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        //3.获取Mapper接口的代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        //4.执行方法
+        List<Brand> brands = brandMapper.selectByCondition(map);
+        System.out.println(brands);
 
         //5.释放资源
         sqlSession.close();
